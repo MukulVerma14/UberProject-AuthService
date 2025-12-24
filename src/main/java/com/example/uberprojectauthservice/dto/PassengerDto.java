59a5d8c1ed0acc9f6a644d699d5a068ed6d88129
MukulVerma14 <mukul23102@iiitnr.edu.in>
2025-12-24 12:52:27 +0530

    Created first plain api with dtos

---
+++ src/main/java/com/example/uberprojectauthservice/dto/PassengerDto.java
@@ -0,0 +1,26 @@
+package com.example.uberprojectauthservice.dto;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class PassengerDto {
+
+    private String id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String phoneNumber;
+
+    private Date createdAt;
+}
---
+++ src/main/java/com/example/uberprojectauthservice/dto/PassengerSignUpRequestDto.java
@@ -0,0 +1,21 @@
+package com.example.uberprojectauthservice.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class PassengerSignUpRequestDto {
+
+    private String email;
+
+    private String password;
+
+    private String phoneNumber;
+
+    private String name;
+
+}
