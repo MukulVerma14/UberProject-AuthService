045ae0189708cd7fc187f7db3088e72bdd71c38a
MukulVerma14 <mukul23102@iiitnr.edu.in>
2025-12-25 16:41:43 +0530

    Added password hashing using bcrypt

--- src/main/java/com/example/uberprojectauthservice/controllers/AuthController.java
+++ src/main/java/com/example/uberprojectauthservice/controllers/AuthController.java
@@ -2,20 +2,30 @@ package com.example.uberprojectauthservice.controllers;
 
 import com.example.uberprojectauthservice.dto.PassengerDto;
 import com.example.uberprojectauthservice.dto.PassengerSignUpRequestDto;
+import com.example.uberprojectauthservice.services.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/auth")
 
 public class AuthController {
+
+    private AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
     @PostMapping("/signup/passenger")
     public ResponseEntity<PassengerDto> signUp(@RequestBody PassengerSignUpRequestDto  passengerSignUpRequestDto) {
+        PassengerDto response = authService.signupPassenger(passengerSignUpRequestDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
 
-        return null;
+    @GetMapping("/signin/passenger")
+    public ResponseEntity<?> signIn() {
+        return new ResponseEntity<>(10, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/example/uberprojectauthservice/services/AuthService.java
+++ src/main/java/com/example/uberprojectauthservice/services/AuthService.java
@@ -1,5 +1,34 @@
 package com.example.uberprojectauthservice.services;
 
-public class AuthService {
+import com.example.uberprojectauthservice.dto.PassengerDto;
+import com.example.uberprojectauthservice.dto.PassengerSignUpRequestDto;
+import com.example.uberprojectauthservice.models.Passenger;
+import com.example.uberprojectauthservice.repositories.PassengerRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
+public class AuthService {
+    private final PassengerRepository passengerRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    public AuthService(PassengerRepository passengerRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.passengerRepository = passengerRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public PassengerDto signupPassenger(PassengerSignUpRequestDto passengerSignUpRequestDto) {
+        Passenger passenger = Passenger.builder()
+                .email(passengerSignUpRequestDto.getEmail())
+                .name(passengerSignUpRequestDto.getName())
+                .password(bCryptPasswordEncoder.encode(passengerSignUpRequestDto.getPassword()))
+                .phoneNumber(passengerSignUpRequestDto.getPhoneNumber())
+                .build();
+
+        Passenger newPassenger = passengerRepository.save(passenger);
+
+       return PassengerDto.from(newPassenger);
+    }
 }
+
