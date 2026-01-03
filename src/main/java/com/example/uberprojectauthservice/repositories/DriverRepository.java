72a4c6b564f4cbd0351682f7295c0e48a9ef7ac4
MukulVerma14 <mukul23102@iiitnr.edu.in>
2026-01-03 18:12:27 +0530

    Refracted models from entity service

---
+++ src/main/java/com/example/uberprojectauthservice/repositories/DriverRepository.java
@@ -0,0 +1,7 @@
+package com.example.uberprojectauthservice.repositories;
+
+import com.example.uberprojectentityservice.models.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DriverRepository extends JpaRepository<Driver, Long> {
+}
